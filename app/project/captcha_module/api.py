c2c210d65f10407f3b13edb4074524fb2d739e27
caiyingyao <cyy0523xc@gmail.com>
2021-06-17 10:27:32 +0800

    使用Depends优化redis连接

--- app/project/captcha_module/api.py
+++ app/project/captcha_module/api.py
@@ -5,7 +5,7 @@
 # Email: cyy0523xc@gmail.com
 # Created Time: 2021-06-16
 # redis连接在公共模块里
-from common.connections import get_redis
+from redis import Redis
 
 # 验证码配置
 cfg = {
@@ -25,36 +25,41 @@ def config(expire=5 * 60, prefix='ctc'):
     cfg['prefix'] = prefix
 
 
-def check_code(token: str, code: str) -> bool:
+def check_code(redis: Redis, token: str, code: str) -> bool:
     """校验验证码"""
     token = f"{cfg['prefix']}_{token}"
     code = code.lower().strip()
-    saved_code = get_redis().get(token)
+    saved_code = redis.get(token)
     if saved_code is None:
         return False
-    get_redis().delete(token)   # 验证码只能用一次
+    redis.delete(token)   # 验证码只能用一次
     saved_code = str(saved_code, encoding='utf8')
     # print(f'{saved_code} == {code}')
     return saved_code == code
 
 
-def set_captcha(token: str, code: str):
+def set_captcha(redis: Redis, token: str, code: str):
     """设置验证码"""
     token = f"{cfg['prefix']}_{token}"
     code = code.lower()
-    return get_redis().set(token, code, ex=cfg['expire'])
+    return redis.set(token, code, ex=cfg['expire'])
 
 
 if __name__ == '__main__':
+    # python api.py 192.168.1.242
     import sys
-    from common.connections import init_redis
+    import os
+    sys.path.insert(0, os.path.split(sys.path[0])[0])
+
+    from common.connections import init_redis, get_redis
     init_redis(host=sys.argv[1])
+    redis = next(get_redis())
 
     code = '23kf'
-    set_captcha('test', code)
-    assert check_code('test', 'test') is False
-    assert check_code('test', code) is False
+    set_captcha(redis, 'test', code)
+    assert check_code(redis, 'test', 'test') is False
+    assert check_code(redis, 'test', code) is False
 
-    set_captcha('test', code)
-    assert check_code('test', code) is True
-    assert check_code('test', code) is False
+    set_captcha(redis, 'test', code)
+    assert check_code(redis, 'test', code) is True
+    assert check_code(redis, 'test', code) is False
--- app/project/common/connections.py
+++ app/project/common/connections.py
@@ -16,14 +16,23 @@ def init_redis(host: str, port=6379, db=0):
     redis_pool = redis.ConnectionPool(host=host, port=port, db=db)
 
 
-def get_redis():
-    """获取redis操作对象"""
-    return redis.Redis(connection_pool=redis_pool)
+def get_redis() -> redis.Redis:
+    """获取redis操作对象
+    每一个请求处理完毕后会关闭当前连接，不同的请求使用不同的连接
+    """
+    # return redis.Redis(connection_pool=redis_pool)
+    r = redis.Redis(connection_pool=redis_pool)
+    try:
+        yield r
+    finally:
+        r.close()
 
 
 if __name__ == '__main__':
     import sys
     init_redis(sys.argv[1])
     r = get_redis()
+    r = next(r)
+    print(r)
     r.set('key', 'val', 10)
     assert str(r.get('key'), encoding='utf8') == 'val'
