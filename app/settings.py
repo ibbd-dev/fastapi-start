b550e7f0e10852cf0ccb7e9ee3ad88c14d8bae7d
caiyingyao <cyy0523xc@gmail.com>
2021-10-27 15:09:04 +0800

    0.6.10: 优化项目名字的长度限制；允许在已有目录中创建项目

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,8 @@ import os
 # 版本
 # 0.6.7: add virtualenv
 # 0.6.8: 优化config命令
-VERSION = "0.6.9"
+# 0.6.10: 优化项目名字的长度限制；允许在已有目录中创建项目
+VERSION = "0.6.10"
 
 # 包跟目录
 package_path = os.path.dirname(os.path.realpath(__file__))
