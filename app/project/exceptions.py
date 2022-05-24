7228d93b797a3fe4e3c639420ddcec0ed23e41bd
caiyingyao <cyy0523xc@gmail.com>
2022-05-24 18:33:32 +0800

    0.6.16: 完善异常响应值

--- app/project/exceptions.py
+++ app/project/exceptions.py
@@ -4,3 +4,24 @@
 # Author: __author__
 # Email: __email__
 # Created Time: __created_time__
+from fastapi import status
+from fastapi.responses import JSONResponse
+from settings import SYSTEM_CODE_BASE
+
+# 状态码基数应该符合这两个条件
+assert SYSTEM_CODE_BASE >= 1000
+assert SYSTEM_CODE_BASE % 1000 == 0
+
+
+class ErrorResponse(JSONResponse):
+    """接口异常响应类型"""
+    def __init__(self, code: int, message: str) -> None:
+        """
+        :param code 响应状态码，取值0-999，若该值大于等于600，则http code会自动重置为500
+        :param message 异常信息
+        """
+        assert 0 <= code < 1000
+        # http的状态码大于600会报错，超过600响应为内部错误
+        status_code = code if code < 600 else status.HTTP_500_INTERNAL_SERVER_ERROR
+        super().__init__(status_code=status_code,
+                         content={"code": SYSTEM_CODE_BASE + code, 'message': message})
--- app/project/settings.py
+++ app/project/settings.py
@@ -7,3 +7,13 @@
 
 # 全局测试状态
 DEBUG = False
+
+# 系统异常状态码的基数
+# 返回给前端的code会加上该值
+SYSTEM_CODE_BASE = 10000
+
+# 数据库连接
+# 在database.py文件中使用
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:password@localhost:3306/test"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
